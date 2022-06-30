afc13b1a12b6622865181d9b5f30c4d5a6956298
ihsankcck <96536577+ihsankcck@users.noreply.github.com>
2022-06-30 04:34:05 +0300

    remove sqlite

--- server.js
+++ server.js
@@ -1,2 +1,5 @@
 const strapi = require("@strapi/strapi");
 strapi().start();
+(PATH=/opt/plesk/node/16/bin:$PATH; npm ci --force && npm run build &> npm.log)
+mkdir -p ./tmp
+touch ./tmp/restart.txt
