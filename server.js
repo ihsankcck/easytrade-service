68c74c519031fc86012a6b75fd046a6eccffabf6
ihsankcck <96536577+ihsankcck@users.noreply.github.com>
2022-06-30 04:34:20 +0300

    update

--- server.js
+++ server.js
@@ -1,5 +1,2 @@
 const strapi = require("@strapi/strapi");
 strapi().start();
-(PATH=/opt/plesk/node/16/bin:$PATH; npm ci --force && npm run build &> npm.log)
-mkdir -p ./tmp
-touch ./tmp/restart.txt
